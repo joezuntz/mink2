169adeca4ff4ff00ff3e067d7efe39da63bac25c
Joe Zuntz <jzuntz@cuillin.roe.ac.uk>
2020-07-24 12:35:07 +0100

    change temp dir on cuillin

--- run-cuillin.sh
+++ run-cuillin.sh
@@ -2,5 +2,9 @@
 # automatically but not in singularity
 source /opt/conda/bin/activate
 
+export TMPDIR=$PWD/temp
+export TEMP=$PWD/temp
+export TMP=$PWD/temp
+
 # run our script - can change this to launch anything
 python run-calculation.py
