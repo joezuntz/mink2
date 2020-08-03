77c63443fe66b668459b62d1beb61c4ebae85945
Nisha Grewal <nishagrewal@Nishas-MacBook-Air-3.local>
2020-08-03 10:24:50 -0700

    added code to run 1 map simulation

--- sky_frac.py
+++ sky_frac.py
@@ -163,3 +163,91 @@ def coefficient_f(thr_ct, smoothing, nside, itr, b, f):
     np.savetxt(os.path.join(sub_path, 'v_all_changing'),V_all)
     
     
+def lensing_f(thr_ct, smoothing, nside, itr, b, f):
+
+    array_len = thr_ct*3
+    
+    print("Fixed Map Iterations:")
+
+    # Fixed map run with iteration count
+    v_all_fixed = np.zeros((itr,array_len)) 
+    for i in range(itr):
+        clustering_maps, lensing_maps = simulate_des_maps(0.3, 0.8, smoothing, nside, nmax=1)
+        v_fixed, v0_fixed, v1_fixed, v2_fixed = calc_mf_f(lensing_maps[0], thr_ct, f, is_clustering=False)
+        v_all_fixed[i] = np.concatenate((v0_fixed,v1_fixed,v2_fixed))
+        print(i)
+    
+    # stack iteration fixed variable versions of V0,V1,V2
+    v_all_mean = np.zeros(array_len)
+    for i in range(array_len):
+        v_all_mean[i] = np.mean(v_all_fixed[:,i])
+        
+        
+    # likelihood perpendicular line points (get constants from plotting notebook)
+    omega_m = np.linspace(0.2,0.4,b)
+    sigma_8 = 0.8989639361571576*omega_m + 0.5303108191528527
+    
+    # calculate S_8
+    S_8 = sigma_8 * (omega_m/0.3)**0.5329788249790618   # exponent value found in plotting notebook
+
+    print("MF Calculations:")
+    
+    # calculate MFs for each omega sigma pair along line
+    v_all = np.zeros((b,array_len))
+
+    for i in range(b):
+        c_map, l_map = simulate_des_maps(omega_m[i], sigma_8[i], smoothing, nside, nmax=1)
+        v,v0,v1,v2 = calc_mf_f(l_map[0], thr_ct, f, is_clustering=False)
+        v_all[i] = np.concatenate((v0,v1,v2))  
+        print(i)
+    
+    # covariance
+    cov = np.cov((v_all_fixed.transpose()))
+
+    # singular covariance matrix workaround
+    good = cov.diagonal() > 0
+    cov2 = cov[good][:, good]
+
+
+    # calculate the likelihood          
+    L = np.zeros(b)
+    N_ = itr-1           # number of realisations - 1
+    p = array_len        # number of data points
+    
+    try:
+        inv_cov = ((N_)/(N_ - p - 1)) * np.linalg.inv(cov)
+        for i in range(b):
+            L[i] = -0.5 * (v_all[i] - v_all_mean) @ inv_cov @ (v_all[i] - v_all_mean)
+    except:
+        inv_cov2 = ((N_)/(N_ - p - 1)) * np.linalg.inv(cov2)
+        for i in range(b):
+            d = (v_all[i] - v_all_mean)[good]
+            L[i] = -0.5 * d @ inv_cov2 @ d
+            
+            
+    coefficient = np.polyfit(S_8,L,2)
+    constraining_power = np.sqrt(-1 / (2*coefficient[0]))
+    
+    # output array
+    c = np.array((thr_ct,smoothing,nside,constraining_power))
+    
+    
+    ## save data ##
+
+    # parent directory
+    output_path = os.path.join(os.getcwd(), '1_Map_Output')
+
+    # save data in new subfolder
+    sub_path = os.path.join(output_path, f't{thr_ct}_n{nside}_s{smoothing}_f{f}')
+    
+
+    try:
+        os.mkdir(sub_path) 
+    except:
+        pass
+
+    np.savetxt(os.path.join(sub_path, 'c.out'),c)
+    np.savetxt(os.path.join(sub_path, 'V_all_fixed.out'),v_all_fixed)
+    np.savetxt(os.path.join(sub_path, 'v_all_changing'),v_all)
+    
+    
