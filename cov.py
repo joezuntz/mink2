858397cdc653aeb2a6f2f12cbc66bb6d9c376f3d
Nisha Grewal <ngrewal@cuillin.roe.ac.uk>
2020-12-02 11:17:50 +0000

    rerun with correct file path

--- cov.py
+++ cov.py
@@ -10,11 +10,13 @@ itr = 1000
 map_count = 270   # 9 maps * threshold count * 3MFs
 v_all = np.zeros((itr,map_count))
 
+e = 0
+
 for i in range(itr):
     try:
-        v_all[i] = np.load(os.path.joinmf_(path,f'V_{i+1}.npy'))
+        v_all[i] = np.load(os.path.join(mf_path,f'V_{i+1}.npy'))
     except:
-        pass
+        e += 1
     
 np.save('v_all',v_all)
-print(v_all)
+print(v_all,'\n, errors=',e)
