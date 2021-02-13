3b2c77eaa5f61ceac35b71798c56cedb8454f1a9
Nisha Grewal <ngrewal@cuillin.roe.ac.uk>
2021-02-13 05:35:07 +0000

    added capability for 1 or 2 map types

--- calc_mf_cl.py
+++ calc_mf_cl.py
@@ -11,6 +11,7 @@ nside = 256
 smoothing_arcmin = 5
 thr_ct = 10
 sky_frac = 1
+m_type = 'c+l' # c+l,c,l
 
 # path for given inputs
 path = '/disk01/ngrewal/Fiducial_Simulations'
@@ -47,12 +48,25 @@ for i in range(len(lmaps)):
 f = int(math.floor(sky_frac*12*nside**2))
 
 # calculate MFs
-v,v0,v1,v2 = calc_mf_2maps(cmaps2,lmaps2,thr_ct,f)
+# clustering and lensing
+if m_type=='c+l':
+    v,v0,v1,v2 = calc_mf_2maps(cmaps2,lmaps2,thr_ct,f)
+    c = Cl_2maps(cmaps2,lmaps2,nside,f)
+
+# clustering
+if m_type=='c':
+    v,v0,v1,v2 = calc_mf_2maps(cmaps2,[],thr_ct,f)
+    c = Cl_2maps(cmaps2,[],nside,f)
+
+# lensing
+if m_type=='l':
+    v,v0,v1,v2 = calc_mf_2maps([],lmaps2,thr_ct,f)
+    c = Cl_2maps([],lmaps2,nside,f)
+
 v_all = np.concatenate((v0.flatten(),v1.flatten(),v2.flatten()))
     
 # calculate Cls
-c = Cl_2maps(cmaps2,lmaps2,nside,f)
 
 # save MFs and Cls
-np.save(os.path.join(path_mf, f'V_{index}_s{smoothing_arcmin}_n{nside}'),v_all)
-np.save(os.path.join(path_cl, f'C_{index}_s{smoothing_arcmin}_n{nside}'),c)
+np.save(os.path.join(path_mf, f'V_{index}_s{smoothing_arcmin}_n{nside}_t{thr_ct}_f{sky_frac)_{m_type}'),v_all)
+np.save(os.path.join(path_cl, f'C_{index}_s{smoothing_arcmin}_n{nside}_t{thr_ct}_f{sky_frac}_{m_type}'),c)
