089491f1432834849a5e46e3ef3106cd3c3e81f8
Nisha Grewal <ngrewal@cuillin.roe.ac.uk>
2021-07-06 18:12:43 +0100

    generating new maps instead of loading and downgrading

--- calc_mf_cl.py
+++ calc_mf_cl.py
@@ -8,32 +8,30 @@ from cl import *
 os.environ["PATH"]='/home/ngrewal/flask/bin:'+os.environ["PATH"]
 
 # define inputs
-nside = 1024
-smoothing_arcmin = 5
-thr_ct = 10
-sky_frac = 1
-m_type = 'c+l' # c+l,c,l
+nside = int(sys.argv[1])#1024
+smoothing_arcmin = int(sys.argv[2])
+thr_ct = int(sys.argv[3])
+sky_frac = int(sys.argv[4])
+m_type = sys.argv[5] #'c+l' 
 
 # path for given inputs
 path = '/disk01/ngrewal/Fiducial_Simulations'
 path_mf = '/disk01/ngrewal/MFs'
 path_cl = '/disk01/ngrewal/Cls'
 
+'''
 # define index of simulation and output
 index = int(os.environ['SLURM_ARRAY_TASK_ID'])
 
 # load maps
 cmaps = np.load(os.path.join(path, f'cmaps_{index}.npy'))  # clustering maps
 lmaps = np.load(os.path.join(path, f'lmaps_{index}.npy'))  # lensing maps
-
  
 ## Simplify Maps ##
 
-# reduce pixel size - don't need to do bc im using nside=1024 in all tests
-#cmaps1 = hp.ud_grade(cmaps,nside,power=-2)         
-#lmaps1 = hp.ud_grade(lmaps,nside)
-cmaps1 = cmaps
-lmaps1 = lmaps
+# reduce pixel size - do second set if using nside=1024 (opt)
+cmaps1 = hp.ud_grade(cmaps,nside,power=-2)         
+lmaps1 = hp.ud_grade(lmaps,nside)
 
 # smooth maps
 smoothing = np.radians(smoothing_arcmin/60)      # convert arcmin to degrees
@@ -45,8 +43,11 @@ for i in range(len(cmaps1)):
  
 for i in range(len(lmaps1)):
     lmaps2[i] = hp.smoothing(lmaps1[i],fwhm=smoothing) 
+'''
 
-    
+## Generate maps using fiducial values
+cmaps2,lmaps2 = simulate_des_maps_bias(0.048,0.3,0.7,0.96,0.8,1.42,1.65,1.6,1.92,2, smoothing, nside)
+
 ## Analysis ##    
 f = int(math.floor(sky_frac*12*nside**2))
 
