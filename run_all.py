b4930b70f48249f9a02da95829d18d07aad05c0e
Nisha Grewal <ngrewal@cuillin.roe.ac.uk>
2021-08-25 16:51:07 +0100

    changed check_output() to run()

--- run_all.py
+++ run_all.py
@@ -5,7 +5,7 @@ Created on Mon Aug 23 16:36:21 2021
 
 @author: ngrewal
 """
-
+import sys
 import subprocess
 
 # DRAFT OF SCRIPT THAT CREATES A SUITE OF SUBMISSION SCRIPTS
@@ -30,14 +30,14 @@ calc_script = open(f'/home/ngrewal/mink2/sub/calc_{a_type}_{m_type}.sub',"w+")
 
 calc_script.write(f'''
                   
-#!/bin/bash                                                                                                                                       
-#SBATCH --time=100:00:00                                                                                                                          
-#SBATCH --array=1-4000                                                                                                                            
-#SBATCH --cpus-per-task=8                                                                                                                         
+#!/bin/bash                                                                                                                                     
+#SBATCH --time=100:00:00                                                                                                                        
+#SBATCH --array=1-4000                                                                                                                          
+#SBATCH --cpus-per-task=8                                                                                                                       
 #SBATCH --exclude={node_list}                                                                 
-#SBATCH --nodes=1                                                                                                                                 
-#SBATCH --tasks-per-node=1                                                                                                                        
-#SBATCH --output='/disk01/ngrewal/logs/log.mf_cl_s{smoothing}_n{nside}_t{thr_ct}_f{sky_frac}_{m_type}.%a.txt'                                                                                        
+#SBATCH --nodes=1                                                                                                                               
+#SBATCH --tasks-per-node=1                                                                                                                      
+#SBATCH --output='/disk01/ngrewal/logs/log.mf_cl_s{smoothing}_n{nside}_t{thr_ct}_f{sky_frac}_{m_type}.%a.txt'                                   
 
 export LD_LIBRARY_PATH=${{CONDA_PREFIX}}/lib:${{LD_LIBRARY_PATH}}
 export OMP_NUM_THREADS=8
@@ -49,40 +49,51 @@ python /home/ngrewal/mink2/fid_concat.py {a_type} {m_type}''')
 calc_script.close()
 
 # run script
-s = subprocess.check_output(args = f'/home/ngrewal/mink2/sub/sbatch calc_{a_type}_{m_type}.sub')
+#s = subprocess.check_output(args = f'sbatch /home/ngrewal/mink2/sub/calc_{a_type}_{m_type}.sub')
+#s = subprocess.check_output('sbatch',f'/home/ngrewal/mink2/sub/calc_{a_type}_{m_type}.sub')
+s = subprocess.run(args = ["f/home/ngrewal/mink2/sub/calc_{a_type}_{m_type}.sub"],capture_output=True)
+#s = subprocess.CompletedProcess(args =  ['sbatch', f'/home/ngrewal/mink2/sub/calc_{a_type}_{m_type}.sub'],returncode=0)
+print(s)
 
 # gets a central job id
 calc_job_id = s.split()[-1]
 print(calc_job_id)
 
+
+'''
 # STEP 3: run an mcmc chain
 
 # create a new script
 mcmc_script = open(f'/home/ngrewal/mink2/sub/mcmc_{a_type}_{m_type}.sub',"w+")
 
 mcmc_script.write(f'''
-                                                                                              
-#!/bin/bash                                                                                                                                       
-#SBATCH --time=05:00:00                                                                                                                           
-#SBATCH --cpus-per-task=32                                                                                                                        
+
+'''
+
+#!/bin/bash                                                                                                                                     
+#SBATCH --time=00:05:00                                                                                                                        
+#SBATCH --cpus-per-task=32                                                                                                                       
 #SBATCH --exclude={node_list}                                                                
-#SBATCH --nodes=1                                                                                                                                 
-#SBATCH --tasks-per-node=1                                                                                                                        
+#SBATCH --nodes=1                                                                                                                                
+#SBATCH --tasks-per-node=1                                                                                                                       
 #SBATCH --output=log.mcmc_{a_type}_{m_type}.txt    
 #SBATCH --dependency=afterok:{calc_job_id}                                                                                                       
 
 export LD_LIBRARY_PATH=${CONDA_PREFIX}/lib:${LD_LIBRARY_PATH}
-export OMP_NUM_THREADS=32
-#time mpiexec -n 1 python mcmc_simplified.py                                                                                                      
-time python /home/ngrewal/mink2/mcmc.py {smoothing} {nside} {thr_ct} {sky_frac} {a_type} {m_type}''')
+export OMP_NUM_THREADS=32                                                                                            
+time python /home/ngrewal/mink2/mcmc.py {smoothing} {nside} {thr_ct} {sky_frac} {a_type} {m_type}'''#)
+
+
+'''
 
 mcmc_script.close()
 
 
 # run script
-s = subprocess.check_output(args = f'/home/ngrewal/mink2/sub/sbatch mcmc_{a_type}_{m_type}.sub')
+s = subprocess.check_output(args = f'sbatch /home/ngrewal/mink2/sub/mcmc_{a_type}_{m_type}.sub')
 
 
 
 
 
+'''
